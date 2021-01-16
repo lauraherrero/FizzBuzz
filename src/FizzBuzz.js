aa11dd30e1390e57f78e55eac374c6c8f95745da
Laura Herrero <lauraherrero36@gmail.com>
2021-01-16 14:25:51 +0100

    Add new file relation with FizzBuzz file

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,22 +1,16 @@
+const number = require('./Number');
+
 function fizzBuzz(num) {
-  if(isMultipleofThree(num) && isMultipleofFive(num)) {
+  if(number.isMultipleofThree(num) && number.isMultipleofFive(num)) {
     return fizz + buzz;
-  } else if(isMultipleofThree(num)) {
+  } else if(number.isMultipleofThree(num)) {
     return fizz;
-  } else if(isMultipleofFive(num)) {
+  } else if(number.isMultipleofFive(num)) {
     return buzz;
   }
 };
 
 const fizz = 'Fizz';
 const buzz = 'Buzz';
-
-function isMultipleofThree(num) {
-  return num % 3 == 0
-};
-
-function isMultipleofFive(num) {
-  return num % 5 == 0
-};
 
 module.exports = fizzBuzz;
