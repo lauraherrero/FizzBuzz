7e353d32801ddec7a83b89a42e3b039b97087c16
Laura Herrero <lauraherrero36@gmail.com>
2021-03-17 18:46:48 +0100

    Add second part of advent of code day 1.

---
+++ advent-of-code/day2.js
@@ -0,0 +1,4 @@
+//Tengo una lista de arrays. En cada uno me dan información de 2 tipos:
+// - La cantidad mínima y máxima que debe estar presente una letra específica.
+// - La serie de números que conforman la contraseña.
+// Averiguar del listado aquellos que cumplen las especificaciones
--- advent-of-code/src/day1.js
+++ advent-of-code/src/day1.js
@@ -1,13 +1,28 @@
 import input from './input-day-1.js';
 
-const adventOfCode = (array) => { // eslint-disable-line consistent-return
-  for (let index = 0; index < array.length; index++) {
-    for (let indexTwo = index + 1; indexTwo < array.length; indexTwo++) {
-      if (parseInt(array[index]) + parseInt(array[indexTwo]) === 2020) {
-        return parseInt(array[index]) * parseInt(array[indexTwo]);
+// const adventOfCode = (array) => { // eslint-disable-line consistent-return
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (parseInt(array[i]) + parseInt(array[j]) === 2020) {
+//         return parseInt(array[i]) * parseInt(array[j]);
+//       }
+//     }
+//   }
+// };
+
+// console.log(adventOfCode(input)); // eslint-disable-line no-console
+
+
+const adventOfCode = (array) => {
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j < array.length; j++) {
+      for ( let k = i + 1; k < array.length; k++) {
+        if (array[i] + array[j] + array[k] === 2020) {
+          return array[i] * array[j] * array[k];
+        }
       }
     }
   }
-};
-
+}
+input = input.map(element => parseInt(element));
 console.log(adventOfCode(input)); // eslint-disable-line no-console
